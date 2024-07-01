783dd30fb57cbf61b81e9c3eed55e14e164f84a5
Eran Leiser <eran1205@gmail.com>
2024-07-01 19:41:47 +0300

    remove password

--- src/main/java/com/my_backend/practice/manageFinance/DBConnection.java
+++ src/main/java/com/my_backend/practice/manageFinance/DBConnection.java
@@ -10,24 +10,6 @@ import java.util.logging.Logger;
 
 @Component
 public class DBConnection  {
-    private static Connection connection = null;
 
-    private static String host = "mysql-26a10081-eran1205-d4b7.a.aivencloud.com";
-    private static String port = "19639";
-
-    public static Connection getConnection() {
-        // String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
-        if (connection == null) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                //connection = DriverManager.getConnection(url, username, password);
-                System.out.println("Connect to Database successfully");
-            } catch (ClassNotFoundException | SQLException ex) {
-                Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        return connection;
-    }
 
 }
